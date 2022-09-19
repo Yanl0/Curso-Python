78dd00372d34088e617559a9fe1e2eb9512eb883
Yanl0 <garciabraza.juanluis@gmail.com>
2022-09-19 11:24:07 +0200

    Final SQLite

--- 10-Bases-Datos/sqlite.py
+++ 10-Bases-Datos/sqlite.py
@@ -15,16 +15,33 @@ cursor.execute("CREATE TABLE IF NOT EXISTS productos("+
 #cursor.execute("INSERT INTO productos VALUES (null, 'Segundo producto', 'Descripcion del producto', 550)")
 #conexion.commit()
 
+#Borrar registros
+cursor.execute("DELETE FROM productos;")
+
+#Insertar muchos registros de una vez
+productos = [
+    ("Ordenador portatil", "buen pc", 700),
+    ("Telefono movil", "buen movil", 140),
+    ("Placa base", "buena placa", 80),
+    ("Tablet", "buen tablet", 300),
+]
+cursor.executemany("INSERT INTO productos VALUES (null,?,?,?)", productos)
+conexion.commit()
+
+#UPDATE
+cursor.execute("UPDATE productos SET Precio=123 WHERE Precio=80")
 #Listar datos
-cursor.execute("SELECT * FROM productos;")
+cursor.execute("SELECT * FROM productos WHERE Precio <= 500;")
 productos = cursor.fetchall()
 for producto in productos:
     print("---------------------")
+    print("ID: ", producto[0])
     print("Titulo: ", producto[1])
     print("Precio: ", producto[3] )
 
 cursor.execute("SELECT Titulo FROM productos;")
 producto = cursor.fetchone()
 print(producto)
+
 #Cerrar conexion
 conexion.close()
