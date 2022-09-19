20658fe320475fa92900ac565d8fb921fd20d497
Yanl0 <garciabraza.juanluis@gmail.com>
2022-09-19 16:56:45 +0200

    Proyecto-Notas V1

---
+++ 11-Proyecto-Notas/main.py
@@ -0,0 +1,25 @@
+"""
+- Abrir asistente
+- Login o registro
+- Si elegimos resgistro creara un usuario en bd
+- Si elegimos login identificara el usuario y preguntara
+- Crear notas, mostrar nostas, borrar notas.
+"""
+
+from usuarios import acciones
+
+print("""
+Acciones disponibles:
+    - registro
+    - login
+""")
+
+hazEl = acciones.Acciones()
+accion = input("Que quiere hacer?: ")
+
+if accion == "registro":
+    hazEl.registro()
+
+elif accion == "login":
+    hazEl.login()
+   
---
+++ 11-Proyecto-Notas/usuarios/acciones.py
@@ -0,0 +1,13 @@
+class Acciones:
+
+    def registro(self):
+        print("\nDe acuerdo!!, vamos a registrarte.")
+        nombre = input("Cual es tu nombre?: ")
+        apellidos = input("Cuales son tus apellidos?: ")
+        email = input("Introduze tu email: ")
+        password = input("Introduze tu contraseña: ")
+    
+    def login(self):
+        print("\n Indentificate.")
+        email = input("Introduze tu email: ")
+        password = input("Introduze tu contraseña: ")
